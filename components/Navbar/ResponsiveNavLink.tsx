f43a18d2af40dbd74f6fadce8564af7605e44cdd
Alferdiyan <ferdi1505@gmail.com>
2023-02-26 02:05:15 +0700

    add type

--- components/Navbar/ResponsiveNavLink.tsx
+++ components/Navbar/ResponsiveNavLink.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-export default function ResponsiveNavLink({ href, children }) {
-    return (
-        <a
-            href={href}
-            className="px-4 py-2 text-sm hover:bg-gray-200 text-gray-700 hover:text-black block"
-        >
-            {children}
-        </a>
-    );
+interface Props {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function ResponsiveNavLink({ href, children }: Props) {
+  return (
+    <a href={href} className="px-4 py-2 text-sm hover:bg-gray-200 text-gray-700 hover:text-black block">
+      {children}
+    </a>
+  );
 }
